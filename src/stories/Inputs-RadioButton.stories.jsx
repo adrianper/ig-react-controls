17e84093f1a13c7c1b1253bccd078981e274c9e4
adrian <adrianperezm.00@gmail.com>
2024-02-02 13:51:48 -0700

    feat: RadioButton component

---
+++ src/stories/Inputs-RadioButton.stories.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+import { RadioButton } from '../components';
+import { customArgTypes, customTableDefaults } from './customArgTypes';
+
+// const variants = { 'filled': 'filled', 'filled--danger': 'filled--danger', 'outlined': 'outlined', 'outlined--danger': 'outlined--danger' };
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+    title: 'Inputs/RadioButton',
+    component: RadioButton,
+    parameters: {
+        // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
+        layout: 'centered',
+        controls: { sort: 'alpha' }
+    },
+    // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+    tags: ['autodocs'],
+    // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+    argTypes: {
+        value: { control: 'none' },
+        onChange: customArgTypes.onChange,
+        direction: {
+            description: '**grid-auto-flow** property',
+            options: ['row', 'column'],
+            table: { type: { summary: "'row' 'column'" } },
+            control: 'radio',
+        },
+        inputs: {
+            description: 'Object with input and label names',
+            table: { type: { summary: `{ "inputName": "label" }` } },
+        },
+        gap: { table: customTableDefaults.string },
+        templateColumns: { table: customTableDefaults.string },
+        templateRows: { table: customTableDefaults.string },
+        likert: { table: { disable: true } },
+    },
+}
+
+export const Example = {
+    args: {
+        inputs: { 'F': 'Femenino', 'M': 'Masculino' },
+        templateColumns: '1fr 1fr',
+        gap: '0.5rem',
+        containerStyle: { direction: 'rtl' },
+        direction: 'row'
+    },
+    render: (args) => {
+        const [value, setValue] = useState('')
+        return <RadioButton {...{ ...args, value, onChange: setValue }} />
+    }
+}
